cea842f186f3188069e96ad70eeb84817b5d1ac6
jjlebow@gmail.com <jlebow@uci.edu>
2019-01-26 13:53:05 -0800

    Scripts for Dialogue
    3 Scripts

---
+++ Odyssey/Assets/Scripts/dialogueManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueManager : MonoBehaviour
+{
+
+	private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Odyssey/Assets/Scripts/dialogueTrigger.cs
+++ Odyssey/Assets/Scripts/dialogueTrigger.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueManager : MonoBehaviour
+public class dialogueTrigger : MonoBehaviour
 {
+
+	public Dialogue dialogue;
+
     // Start is called before the first frame update
     void Start()
     {
